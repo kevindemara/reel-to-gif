b5d62d8405590bb2c131821573c48d068be34054
Kevin DeMara <kevin.demara@gmail.com>
2025-03-27 21:28:20 -0400

    Remove duplicate index.html from root directory

--- app.py
+++ app.py
@@ -98,3 +98,7 @@ def index():
             return f"<h3>Error: {str(e)}</h3><pre>{debug}</pre>"
 
     return render_template("index.html")
+    
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
